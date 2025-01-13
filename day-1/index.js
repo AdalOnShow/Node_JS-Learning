f52fdd3121a597041dff591447395add2c448436
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-13 21:04:19 +0600

    first commit

---
+++ day-1/index.js
@@ -0,0 +1,12 @@
+// console.log("Hello World");
+
+// const student1 = require('./student')
+
+// console.log(student1.getName());
+// console.log(student1.getAge());
+// // console.log(student1.result);
+// console.log(student1.cgpa);
+
+const { getName } = require('./student')
+
+console.log(getName());
---
+++ day-1/student.js
@@ -0,0 +1,19 @@
+const getName = () => {
+  return "Sharif Adal";
+}
+
+const getAge = () => {
+  return 17;
+}
+
+const cgpa = 3.8;
+
+// exports.getName = getName;
+// exports.getAge = getAge;
+// exports.result = cgpa;
+
+module.exports = {
+  getName,
+  getAge,
+  cgpa
+}
