fcf0d8711a9c219abc592a9d51f6e6817aad100a
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-19 09:22:24 +0600

    Refactor module.js to include path operations and comment out memory logs

--- day-2/modules.js
+++ day-2/modules.js
@@ -7,5 +7,16 @@ const os = require("os");
 
 const {totalmem, freemem} = require("os");
 
-console.log(totalmem())
-console.log(freemem())
+// console.log(totalmem())
+// console.log(freemem())
+
+// console.log(__dirname)
+// console.log(__filename)
+
+const path = require("path");
+
+const extensionName = path.extname(__filename);
+// console.log(extensionName);
+
+const joinName = path.join(__dirname, "/../views.js");
+console.log(joinName);
