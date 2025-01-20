b08bc038c141424b6fda28c24a4a58e18c301cec
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-01-20 11:03:10 +0600

    Refactor server.js to define server hostname and port, and improve server startup logging

--- day-2/server.js
+++ day-2/server.js
@@ -1,5 +1,20 @@
-const http = require("http")
+// const http = require("http")
 
-http.createServer((req, res) => {
+// http.createServer((req, res) => {
+//   res.end("Hello! I am your first server");
+// }).listen(3000)
+
+
+
+
+const http = require("http")
+const prot = 3000;
+const hostname = '127.0.0.1';
+
+const myServer = http.createServer((req, res) => {
   res.end("Hello! I am your first server");
-}).listen(3000)
+});
+
+myServer.listen(prot, hostname, () => {
+  console.log(`Server is running successfully on http://${hostname}:${prot}`);
+})
