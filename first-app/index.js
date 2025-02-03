189fac5a822d4e3e6ec3eaed2c3610a03c7fe2ee
AdalOnShow <mdadalbadsha2008@gmail.com>
2025-02-03 14:42:02 +0600

    Add HTML pages and implement file reading for server routes

--- first-app/index.js
+++ first-app/index.js
@@ -2,8 +2,28 @@ const http = require("http");
 const fs = require("fs");
 const PORT = 3000;
 
+const handelReadFile = (filename, statusCode, res) => {
+  fs.readFile(filename, "utf-8", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.writeHead(statusCode, { "Content-Type": "text/html" });
+      res.write(data);
+      res.end();
+    }
+  })
+}
+
 const server = http.createServer((req, res) => {
-  res.end("Hello World");
+  if (req.url === "/") {
+    handelReadFile("views/index.html", 200, res);
+  } else if (req.url === "/about") {
+    handelReadFile("views/about.html", 200, res);
+  } else if (req.url === "/contact") {
+    handelReadFile("views/contact.html", 200, res);
+  } else {
+    handelReadFile("views/404.html", 404, res);
+  }
 })
 
 server.listen(PORT, () => {
